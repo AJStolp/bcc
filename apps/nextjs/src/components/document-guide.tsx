96ea02e2fcdcf285a1056869f0d526407a7556f1
Anthony Stolp <anthony.stolp@gmail.com>
2024-08-07 17:39:26 -0400

    emoji change to wood

--- apps/nextjs/src/components/document-guide.tsx
+++ apps/nextjs/src/components/document-guide.tsx
@@ -7,7 +7,7 @@ import { AnimatedGradientText } from "@saasfly/ui/animated-gradient-text";
 export function DocumentGuide({ children }: { children: ReactNode }) {
   return (
     <AnimatedGradientText>
-      🚀 <hr className="mx-2 h-4 w-[1px] shrink-0 bg-gray-300" />{" "}
+      🪵 <hr className="mx-2 h-4 w-[1px] shrink-0 bg-gray-300" />{" "}
       <span
         className={cn(
           `animate-gradient inline bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
